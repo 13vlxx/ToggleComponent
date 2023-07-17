75d119ec688e772f61821e7228a87c44883701fd
ALX <alexmonac13@gmail.com>
2023-07-18 00:59:44 +0200

    Initial Commit

---
+++ ToggleComponent/ToggleComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleComponentApp.swift
+//  ToggleComponent
+//
+//  Created by Alex on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToggleComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
