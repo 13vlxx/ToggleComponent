fc4237f92d7213895e06ee556d383ae30b2f4bac
ALX <alexmonac13@gmail.com>
2023-07-18 01:23:08 +0200

    Swift toggle component

--- ToggleComponent/ContentView.swift
+++ ToggleComponent/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isOptionActivated = false
+    
+    @State private var isFavoriteActivated = false
+    @State private var isTrashActivated = false
+    @State private var isSendActivated = false
+    @State private var isDisturbActivated = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            // Section pour le premier toggle
+            Section {
+                Toggle(isOn: $isOptionActivated, label: {
+                    Text("Voir les options")
+                })
+            }
+            
+            if isOptionActivated {
+                // Section pour les toggles affichés par la suite
+                Section {
+                    Toggle(isOn: $isFavoriteActivated, label: {
+                        HStack {
+                            Image(systemName: "star.fill").foregroundColor(.yellow)
+                            Text("Favoris")
+                        }
+                    })
+                    
+                    Toggle(isOn: $isTrashActivated, label: {
+                        HStack {
+                            Image(systemName: "trash.fill").foregroundColor(.red)
+                            Text("Corbeille")
+                        }
+                    })
+                    
+                    Toggle(isOn: $isSendActivated, label: {
+                        HStack {
+                            Image(systemName: "paperplane.fill").foregroundColor(.blue)
+                            Text("Envoi")
+                        }
+                    })
+                    
+                    Toggle(isOn: $isDisturbActivated, label: {
+                        HStack {
+                            Image(systemName: "moon.fill").foregroundColor(.purple)
+                            Text("Ne pas déranger")
+                        }
+                    })
+                }
+            }
         }
-        .padding()
     }
 }
 
